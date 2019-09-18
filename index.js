314d7beab4f54f5ad0ad22dbe23303209f660904
Antti Heimonen <anttiheimonen@hotmail.com>
2019-09-18 16:39:07 +0300

    Puhelinluettelon backend step 3 valmis

--- index.js
+++ index.js
@@ -28,9 +28,17 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  // Find person with given id number
+  const person = persons.find(person => person.id === id)
+  // If person found, return it. Otherwise return status 404
+  person !== undefined ? res.json(person) : res.status(404).end()
+})
+
 app.get('/info', (req, res) => {
   res.send(`<p>Phonebook has info for ${persons.length} people</p>` +
-  `<p>${new Date()}</p>`
+           `<p>${new Date()}</p>`
   )
 })
 
